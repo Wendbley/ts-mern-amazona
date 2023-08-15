f1f91e4d89b4d0f7770395852822bcea6fb9b499
Johnny Wendbley <johnnywendbley@outlook.com>
2023-08-15 17:25:38 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+#TypeScript MERN AMAZONA
+
+##Lessons
+1. Introduciton
+2. Install Tools
+3. Create TypeScript React App by Vite
+4. Create Git Repository
---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+      <header>
+        TS Amazona
+      </header>
+      <main></main>
+      <footer>
+        All rights Reseved
+      </footer>
+    </>
+  )
+}
+
+export default App
